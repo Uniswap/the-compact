dff998d4539ad0ef50c7cdd18d51b3d1bd457ab2
mgretzke <m.gretzke@vimage.de>
2025-11-12 17:05:56 +0100

    changed stateless lib to helper contract

--- src/utility/Utility.sol
+++ src/utility/Utility.sol
@@ -5,14 +5,20 @@ import { Extsload } from "../lib/Extsload.sol";
 import { ERC6909 } from "solady/tokens/ERC6909.sol";
 import { Tstorish } from "../lib/Tstorish.sol";
 
-library UtilityLib {
+contract Utility {
     address internal constant THE_COMPACT = address(0x00000000000000171ede64904551eeDF3C6C9788);
     address internal constant TSTORE_TEST_CONTRACT = address(0x627c1071d6A691688938Bb856659768398262690);
 
     uint256 internal constant REENTRANCY_GUARD_SLOT = 0x929eee149b4bd21268;
 
+    bool internal immutable TSTORE_INITIAL_SUPPORT;
+
     error TheCompactNotDeployed();
     error BalanceNotSettled();
+
+    constructor() {
+        TSTORE_INITIAL_SUPPORT = checkTstoreAvailable();
+    }
 
     /// @notice Checks if the Compact is deployed and if transient storage is available on the chain.
     function checkTstoreAvailable() internal view returns (bool ok) {
@@ -33,21 +39,23 @@ library UtilityLib {
     /// @notice Returns the users balance only if reentrancy protection is not active on the Compact. This eliminates in flight balances before the ERC6909 tokens were burned.
     /// @dev The function favors chains supporting eip-1153 (transient storage)
     function settledBalanceOf(address owner, uint256 id) internal view returns (uint256 amount) {
-        // Favoring transient storage
-        try Extsload(THE_COMPACT).exttload(bytes32(REENTRANCY_GUARD_SLOT)) returns (bytes32 reentrancySlotContent) {
-            if (uint256(reentrancySlotContent) > 1) {
-                revert BalanceNotSettled();
-            }
-        } catch {
-            // If the call fails, assume transient storage is not available, so falling back to persistent storage
-            try Extsload(THE_COMPACT).extsload(bytes32(REENTRANCY_GUARD_SLOT)) returns (bytes32 reentrancySlotContent) {
-                if (uint256(reentrancySlotContent) > 1) {
-                    revert BalanceNotSettled();
-                }
-            } catch {
-                // If the call fails as well, assume the compact is not deployed
-                revert TheCompactNotDeployed();
-            }
+        bytes32 reentrancySlotContent;
+
+        if (TSTORE_INITIAL_SUPPORT) {
+            // Only check the tstore reentrancy guard slot
+            reentrancySlotContent = Extsload(THE_COMPACT).exttload(bytes32(REENTRANCY_GUARD_SLOT));
+        } else {
+            // Check both slots to cover the potential transition period
+            try Extsload(THE_COMPACT).exttload(bytes32(REENTRANCY_GUARD_SLOT)) returns (bytes32 content) {
+                reentrancySlotContent = content;
+            } catch { }
+
+            // Independent of the result, check the persistent storage slot
+            reentrancySlotContent |= Extsload(THE_COMPACT).extsload(bytes32(REENTRANCY_GUARD_SLOT));
+        }
+
+        if (uint256(reentrancySlotContent) > 1) {
+            revert BalanceNotSettled();
         }
 
         // If we get here, the balance is settled, so returning the balance
--- test/utility/UtilityLib.t.sol
+++ test/utility/UtilityLib.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { UtilityLib } from "../../src/utility/UtilityLib.sol";
+import { Utility } from "../../src/utility/Utility.sol";
 import { TheCompact } from "../../src/TheCompact.sol";
 import { ERC20 } from "solady/tokens/ERC20.sol";
 
